64952b15ed362ca95b223b2b215d02ecf554bf63
AinoVakkilainen <ainoeveliina.vakkilainen@gmail.com>
2024-12-16 17:54:31 +0200

    Update App.java
    Aino Vakkilaisen commit tehtävään

--- git-tehtava-ryhma-21/src/App.java
+++ git-tehtava-ryhma-21/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Ville devaa");
+        input("Aino");
+    }
+        
+    public static void input(String nimi) {
+        System.out.println(nimi + "n input tehtävään.");
     }
 }
