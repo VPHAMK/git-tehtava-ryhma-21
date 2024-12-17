96ef5ac2da7e533ce8d4117dcc451c063a4403f6
VPHAMK <amk1004741@student.hamk.fi>
2024-12-17 07:52:20 +0200

    Update App.java
    Kommentoitu koodi

--- git-tehtava-ryhma-21/src/App.java
+++ git-tehtava-ryhma-21/src/App.java
@@ -1,9 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ville devaa");
+        System.out.println("Ville devaa"); //en nyt tiedä voiko tätä devaamiseksi kutsua
         input("Aino");
     }
-        
+    
+    //hieno metodi
     public static void input(String nimi) {
         System.out.println(nimi + "n input tehtävään.");
     }
